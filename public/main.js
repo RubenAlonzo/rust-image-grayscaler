c1cadc707f9c307a193d9f4d62d5e2fc2b9da52b
Ruben <ralonzo@cyberwarrior.com>
2023-01-25 07:37:43 -0400

    Consume image as string

--- public/main.js
+++ public/main.js
@@ -1,1 +1,18 @@
-console.log('test');
+function init(){
+  const input = document.getElementById('upload')
+  const fileReader = new FileReader()
+
+  fileReader.onloadend = () => {
+    let base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/, ''
+    )    
+    console.log(base64)
+  }
+
+  input.addEventListener('change', () => {
+    // read image as string to transfer easier over languages
+    fileReader.readAsDataURL(input.files[0])
+  })
+}
+
+init()
